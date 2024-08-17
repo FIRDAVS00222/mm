cc1a114be0ed6f189a69cc1ca1208fcd4d749b2c
FIRDAVS00222 <shoirajamilova5@gmail.com>
2024-08-17 13:49:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let str = prompt('String typedagi malumotni kiriting?')
+console.log(typeof str);
+let str1 =prompt('Number typedagi malumotni kiriting?') 
+console.log(typeof str1);
+let str3 = prompt('boollean typedagi malumotni kiriting?')
+console.log(typeof true);
+let str4 =confirm('Hamma malumotlarni togri kiritganingizga ishonchingiz  komilmi?')
+console.log( str4 );
